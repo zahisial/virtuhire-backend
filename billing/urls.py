134f14ec022436281c826334ba3d7b5fb35b76b2
zahisial <shersials@gmail.com>
2026-03-22 02:35:30 +0500

    Fix gitignore and remove .env tracking

--- billing/urls.py
+++ billing/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('invoices/',              views.invoices,              name='invoices'),
     path('payments/',              views.payments,              name='payments'),
     path('create-payment-intent/', views.create_payment_intent, name='create-payment-intent'),
+    path('save-card/',             views.save_card,             name='save-card'),
     path('webhook/',               views.stripe_webhook,        name='stripe-webhook'),
 ]
