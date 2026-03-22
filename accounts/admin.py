c7a7cfd236c5191f29265b8fe62cff2d55be11a0
zahisial <shersials@gmail.com>
2026-03-23 01:27:32 +0500

    Fix settings syntax and admin registration

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,21 @@
-# virtuhire-backend/accounts/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, ClientProfile, OTP
 
-admin.site.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ['email']
+    list_display = ['email', 'role', 'is_staff', 'is_active']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Info', {'fields': ('phone', 'role')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {'classes': ('wide',), 'fields': ('email', 'password1', 'password2', 'role', 'is_staff', 'is_superuser')}),
+    )
+    search_fields = ['email']
+    filter_horizontal = ('groups', 'user_permissions')
+
+admin.site.register(User, UserAdmin)
 admin.site.register(ClientProfile)
 admin.site.register(OTP)
